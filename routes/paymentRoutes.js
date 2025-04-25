403238b0e0078ff217d4727825379b939244fe93
Abhinav Sharma <0011abhinav@gmail.com>
2025-04-25 19:49:57 +0530

    Remove all files from abhinav-sharma branch before new push

--- routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { processStripePayment } = require('../controllers/paymentController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.route('/stripe').post(protect, processStripePayment);
-
-module.exports = router;
